7d5fc8914b7d786adbbcb46e5f09fe3f40fb0d46
Roman Antonyuk <nedavaysvoilogi@yandex.ru>
2023-03-13 18:09:10 +0300

    Разделил код на модули

---
+++ js/data.js
@@ -0,0 +1,55 @@
+import {getRandomInteger} from './util.js';
+import {getRandomArrayElement} from './util.js';
+import {generateCommentId} from './util.js';
+
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCRIPTIONS = [
+  'Лето 2013',
+  'Пляжная вечеринка',
+  'Снежные горы',
+  'Аттракцион невиданной щедрости',
+  'Скачки на слонах',
+];
+
+const NAMES = [
+  'Вася',
+  'Рома',
+  'Ксюша',
+  'Федя',
+  'Карина',
+];
+
+const createMessage = () =>
+  Array.from({ length: getRandomInteger(1, 2) }, () =>
+    getRandomArrayElement(MESSAGES)
+  ).join(' ');
+
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
+
+const createPicture = (index) => ({
+  id: index,
+  url: `photo/${index}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({length: getRandomInteger(0, 5)}, createComment),
+});
+
+const getPictures = () => Array.from({length: 25}, (_, pictureIndex) =>
+  createPicture(pictureIndex + 1)
+);
+
+export {getPictures};
---
+++ js/util.js
@@ -0,0 +1,27 @@
+
+//Функция для генерации случайного числа
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export {getRandomInteger};
+
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+export {getRandomArrayElement};
+
+const createIdGenerator = () => {
+  let lastGenerateId = 0;
+
+  return () => {
+    lastGenerateId += 1;
+    return lastGenerateId;
+  };
+};
+
+const generateCommentId = createIdGenerator();
+
+export {generateCommentId};
